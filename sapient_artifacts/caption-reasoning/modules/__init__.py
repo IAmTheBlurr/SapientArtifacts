6db9e3a55c307ea49fab1e26b8c6d4224ec07a00
Blurr <blurr@iamtheblurr.com>
2025-06-08 00:45:03 -0700

    modules/__init__.py -> adds newest modules to package level imports

--- sapient_artifacts/caption-reasoning/modules/__init__.py
+++ sapient_artifacts/caption-reasoning/modules/__init__.py
@@ -1,3 +1,5 @@
 """ This module is used to import all the classes in the lib module. """
+from .conversation_parser import ConversationParser
 from .target_video import TargetVideo
+from .text_formatter import TextFormatter
 from .webvtt import WebVTT
